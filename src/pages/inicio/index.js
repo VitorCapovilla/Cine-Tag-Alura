ed1525968ac321457850ead546f33ec56539f5c7
VitorCapovillaMast <vitor.capovilla@mastsolucoes.com>
2024-09-04 16:43:15 -0300

    Banner: Importação para a página inicial

--- src/pages/inicio/index.js
+++ src/pages/inicio/index.js
@@ -1,11 +1,13 @@
 import Cabecalho from 'components/Cabecalho';
 import Rodape from 'components/Rodape';
 import './Inicio.module.css';
+import Banner from 'components/Banner';
 
 const Inicio = () => {
     return (
         <>
             <Cabecalho/>
+            <Banner imagem="home"/>
             <Rodape/>
         </>
     )
