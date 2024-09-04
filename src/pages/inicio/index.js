9ca6631a0b82d3342b90fb8d30cad5f4d2c8d173
VitorCapovillaMast <vitor.capovilla@mastsolucoes.com>
2024-09-04 16:50:11 -0300

    Título: Importação do componente título na página inicial

--- src/pages/inicio/index.js
+++ src/pages/inicio/index.js
@@ -2,12 +2,14 @@ import Cabecalho from 'components/Cabecalho';
 import Rodape from 'components/Rodape';
 import './Inicio.module.css';
 import Banner from 'components/Banner';
+import Titulo from 'components/Titulo';
 
 const Inicio = () => {
     return (
         <>
             <Cabecalho/>
             <Banner imagem="home"/>
+            <Titulo>Teste</Titulo>
             <Rodape/>
         </>
     )
