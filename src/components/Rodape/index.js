965e6e6f0052f396a80ac340e7ab2c92feed3a45
VitorCapovillaMast <vitor.capovilla@mastsolucoes.com>
2024-09-04 16:32:05 -0300

    Rodape: Desenvolvimento e estilização do componente

---
+++ src/components/Rodape/index.js
@@ -0,0 +1,11 @@
+import styles from './Rodape.module.css';
+
+const Rodape = () => {
+    return (
+        <div className={styles.Footer}>
+            <h2>Desenvolvido por Vitor Capovilla</h2>
+        </div>
+    )
+}
+
+export default Rodape
